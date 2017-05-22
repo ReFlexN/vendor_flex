9973dbe7bb13390f2426c23077075469f62d15e3
Akhil Narang <akhilnarang.1999@gmail.com>
2017-05-22 03:24:33 +0200

    vendor: cleanup derp props
    Change-Id: Ia45e777af8666e4a66f10365801229bf0c665c2c
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- version.mk
+++ version.mk
@@ -1,2 +1,6 @@
 VERSION := v1.0
-REFLEX_VERSION ?= reflex-$(VERSION)-$(TARGET_DEVICE)-$(PLATFORM_VERSION)-$(shell date +%Y%m%d)
+REFLEX_VERSION ?= ReFlex-$(VERSION)-$(TARGET_DEVICE)-$(PLATFORM_VERSION)-$(shell date +%Y%m%d)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.caf.version=$(shell grep "<default revision=" .repo/manifest.xml | awk -F'"' '{print $$2}' | awk  -F "/" '{print $$3}') \
+    ro.reflex.version=$(REFLEX_VERSION)
