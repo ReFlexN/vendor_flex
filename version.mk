abc39fb7e97f1432d90a3904911fa8fd5c00f5fc
Akhil Narang <akhilnarang.1999@gmail.com>
2017-07-15 20:23:30 +0200

    reflex: add buildtype stuff [1/2]
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
 VERSION := v1.0
-REFLEX_VERSION ?= ReFlex-$(VERSION)-$(TARGET_DEVICE)-$(PLATFORM_VERSION)-$(shell date +%Y%m%d)
+REFLEX_BUILDTYPE ?= Unofficial
+REFLEX_VERSION ?= ReFlex-$(VERSION)-$(TARGET_DEVICE)-$(PLATFORM_VERSION)-$(REFLEX_BUILDTYPE)-$(shell date +%Y%m%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.caf.version=$(shell grep "<default revision=" .repo/manifest.xml | awk -F'"' '{print $$2}' | awk  -F "/" '{print $$3}') \
